c75257fb3c1c98137e6958e65e4ab237cf10b8c3
capturemathan <capturesmk@gmail.com>
2020-09-22 19:38:46 +0530

    Adds Bulma :wink:

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="tag">Hello</span>
+    </div>
+  );
+}
+
+export default App;
