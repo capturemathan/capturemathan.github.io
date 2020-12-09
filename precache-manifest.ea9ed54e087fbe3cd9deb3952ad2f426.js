d96cae96916911ac33b216cf1722b9c20744ba53
capturemathan <capturesmk@gmail.com>
2020-12-09 12:21:32 +0530

    Updates

---
+++ precache-manifest.ea9ed54e087fbe3cd9deb3952ad2f426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e00c98944286d8eae7afaf744475fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ccab92489fa065f174",
+    "url": "/static/css/main.3fcdc186.chunk.css"
+  },
+  {
+    "revision": "08d760f227778ffd31c2",
+    "url": "/static/js/2.2613fe99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2613fe99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ccab92489fa065f174",
+    "url": "/static/js/main.a4c3a2a9.chunk.js"
+  },
+  {
+    "revision": "6054be5de025b692a00c",
+    "url": "/static/js/runtime-main.beddf7b2.js"
+  }
+]);
