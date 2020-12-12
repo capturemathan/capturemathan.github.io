48d5b38969d6bde7033e675d8e60b460ca617c93
capturemathan <capturesmk@gmail.com>
2020-12-12 20:43:43 +0530

    Formats Code :recycle:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import Typist from 'react-typist';
+import React from "react";
+import Typist from "react-typist";
 
-class Navbar extends React.Component{
-    render(){
-        return (
-            <nav className="navbar is-fixed-top">
-                <div className="navbar-brand">
-                    <Typist>
-                    Mathan's Folio
-                    </Typist>
-                </div>
-            </nav>
-        );
-    }
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar is-fixed-top">
+        <div className="navbar-brand">
+          <Typist>Mathan's Folio</Typist>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
